82117bdbd261697f83fe4d9c514febf11dfdf357
veragrosskop <veragrosskop@gmail.com>
2026-06-01 12:47:03 +0200

    -converted content field to html text area -added input validation - added global path for json file - prompt for confirmation on delete

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
+JSON_BLOG_FILE_PATH = "blog_posts.json"
 
 def read_blogs_from_json(json_file_path) -> List[Dict]:
     """Loads a JSON file and returns a dictionary of data"""
@@ -22,11 +23,14 @@ def write_blogs_to_json(json_file_path: str, data: List[Dict]) -> None:
 
 def get_next_id(blog_posts) -> int:
     """Returns the next post id (not accounting for gap id's in existing posts)."""
-    max_id = max(post["id"] for post in blog_posts)
+    if len(blog_posts) == 0:
+        return 1
+    else:
+        max_id = max(post["id"] for post in blog_posts)
     return max_id + 1
 
 
-def get_post_by_id(blog_posts, post_id) -> Tuple[int, Dict] | Tuple[None, None]:
+def get_post_by_id(blog_posts, post_id):
     for i, post in enumerate(blog_posts):
         if post["id"] == post_id:
             return i, post
@@ -60,7 +64,7 @@ def remove_post(post_id, json_file_path):
 
 @app.route("/")
 def index():
-    blog_posts = read_blogs_from_json("blog_posts.json")
+    blog_posts = read_blogs_from_json(JSON_BLOG_FILE_PATH)
     return render_template("index.html", blog_posts=blog_posts)
 
 
@@ -69,32 +73,66 @@ def add():
     error_msg = ""
     json_file_path = "blog_posts.json"
     if request.method == "POST":
+        is_valid_author = False
+        is_valid_title = False
+        is_valid_content = False
+
         # Grab data from form
-        author = request.form["author"]
-        title = request.form["title"]
-        content = request.form["content"]
+        author_input = request.form["author"].strip()
+        if author_input == "":
+            author = "Anonymous"
+            is_valid_author = True
+        elif not author_input.isalpha():
+            error_msg += "Please fill in an author, with alphabetical characters only or leave blank for Anonymous."
+        elif len(author_input) > 0:
+            author = author_input
+            is_valid_author = True
+
+        title_input = request.form["title"].strip()
+        if (not title_input.isalpha()) or title_input == "":
+            error_msg += " Please fill in a title, with alphabetical characters only."
+        elif len(title_input) > 0:
+            title = title_input
+            is_valid_title = True
+
+        content_input = request.form["content"].strip()
+        if content_input == "":
+            error_msg += " Please fill in a content."
+        elif len(content_input) > 0:
+            content = content_input
+            is_valid_content = True
 
         # validate form data
-        if len(author) > 0 and len(title) > 0 and len(content) > 0:
+        if is_valid_author and  is_valid_title and is_valid_content:
             # continue to homepage if valid
-            add_post(author, title, content, "blog_posts.json")
+            add_post(author, title, content, JSON_BLOG_FILE_PATH)
             return redirect("/")
         else:
             # display error message
-            error_msg = "Please fill in author, title, and content"
+            if error_msg == "":
+                error_msg = "Please fill in a valid author, title, and content"
     return render_template("add_form.html", message=error_msg)
 
 
-@app.route("/delete/<int:post_id>", methods=["POST"])
+@app.route("/delete/<int:post_id>", methods=["POST", "GET"])
 def delete(post_id):
-    remove_post(post_id=post_id, json_file_path="blog_posts.json")
-    return redirect("/")
+    blog_posts = read_blogs_from_json(JSON_BLOG_FILE_PATH)
+    _, post = get_post_by_id(blog_posts, post_id)
+
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == "POST":
+        remove_post(post_id=post_id, json_file_path=JSON_BLOG_FILE_PATH)
+        return redirect("/")
+
+    return render_template("confirm_delete.html", post=post)
 
 
 @app.route("/update/<int:post_id>", methods=["POST", "GET"])
 def update(post_id):
     error_msg = ""
-    json_file_path = "blog_posts.json"
+    json_file_path = JSON_BLOG_FILE_PATH
     blog_posts = read_blogs_from_json(json_file_path)
     i, post = get_post_by_id(blog_posts, post_id)
     if post is None or i is None:
